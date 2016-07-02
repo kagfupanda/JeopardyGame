15edc3f4a965918d0fc35e0f68e316affa908383
sriniv <sriniv@virtuallycontinuous.com>
2016-07-02 11:26:52 -0700

    add comment, change jframe title

--- JepordyGame/src/my/JeapordyGame.java
+++ JepordyGame/src/my/JeapordyGame.java
@@ -8,11 +8,12 @@ import javax.swing.JFrame;
 
 public class JeapordyGame {
 	// did commit
+	// made a change - testing git
 	
 	public static void main(String[] args) {
 		ArrayList<String> cats = new ArrayList<>(Arrays.asList("Biogeochemical-Cycles", "Biotic-Or-Abiotic", "Ecological-Restoration", "Population", "Water-Pollution"));
 		ArrayList<String> dollarValues = new ArrayList<>(Arrays.asList("100", "200", "300", "400", "500"));
-		JFrame jf = new JFrame("JeopardyGame");
+		JFrame jf = new JFrame("JeopardyGame - Update from srinivirt");
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ScorePanel sp = new ScorePanel();
 		CategoryManagerPanel cmp = new CategoryManagerPanel(cats, dollarValues, sp);
